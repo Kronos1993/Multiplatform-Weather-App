a3deb352475af12c7bbbda1b9e7f6e5f32ebb15b
kronos <mguerral1993@gmail.com>
2025-10-26 21:20:59 -0500

    feat: change ios app language

--- composeApp/src/iosMain/kotlin/com/kronos/multiplatform/weatherapp/core/widget/WidgetUpdater.ios.kt
+++ composeApp/src/iosMain/kotlin/com/kronos/multiplatform/weatherapp/core/widget/WidgetUpdater.ios.kt
@@ -2,6 +2,5 @@ package com.kronos.multiplatform.weatherapp.core.widget
 
 actual class WidgetUpdater : IWidgetUpdater {
     override suspend fun updateAllWeatherWidgets() {
-        TODO("Not yet implemented")
     }
 }
