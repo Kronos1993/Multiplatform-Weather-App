10d6ec21b32d062ee4cd1b2586cd2998cd13034e
kronos <mguerral1993@gmail.com>
2025-10-29 09:38:20 -0500

    fix: snackbar text color updated

--- composeApp/src/commonMain/kotlin/com/kronos/multiplatform/weatherapp/features/home/current_weather/WeatherScreen.kt
+++ composeApp/src/commonMain/kotlin/com/kronos/multiplatform/weatherapp/features/home/current_weather/WeatherScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.kronos.multiplatform.weatherapp.components.LoadingDialog
@@ -122,7 +123,7 @@ fun WeatherScreen(
                             data.visuals.message.contains("error", ignoreCase = true) ->
                                 MaterialTheme.colorScheme.onError
 
-                            else -> MaterialTheme.colorScheme.onPrimary
+                            else -> Color.White
                         }
                     )
                 }
