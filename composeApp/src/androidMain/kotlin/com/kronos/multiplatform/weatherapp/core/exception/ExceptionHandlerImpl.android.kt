46881e935b8baa67326768ff3a989c274c473b75
kronos <mguerral1993@gmail.com>
2025-11-01 17:53:56 -0500

    feat: logger added

--- composeApp/src/androidMain/kotlin/com/kronos/multiplatform/weatherapp/core/exception/ExceptionHandlerImpl.android.kt
+++ composeApp/src/androidMain/kotlin/com/kronos/multiplatform/weatherapp/core/exception/ExceptionHandlerImpl.android.kt
@@ -2,18 +2,10 @@ package com.kronos.multiplatform.weatherapp.core.exception
 
 import android.content.Context
 import android.util.Log
-import com.kronos.multiplatform.weatherapp.core.logguer.ILogManager
-import com.kronos.multiplatform.weatherapp.core.logguer.LogLevel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
 
 actual class ExceptionHandlerImpl(
-    private val context: Context,
-    var logguer: ILogManager
+    private val context: Context
 ) : ExceptionHandler,Thread.UncaughtExceptionHandler {
-
-    private val TAG = "Weather-App Exception"
-
     private var mDefaultHandler: Thread.UncaughtExceptionHandler? = null
 
     override fun init() {
@@ -23,8 +15,5 @@ actual class ExceptionHandlerImpl(
 
     override fun uncaughtException(t: Thread, e: Throwable) {
         Log.e(this.javaClass.name, "uncaughtException: ", e)
-        runBlocking(Dispatchers.IO) {
-            logguer.log(LogLevel.ERROR, TAG, "uncaughtException: ${e.message.orEmpty()}")
-        }
     }
 }
