56c116b2c0f16fc8aa239eb3917121f137eb29f9
kronos <mguerral1993@gmail.com>
2026-01-11 16:11:41 -0500

    feat: toggle measure unit btw metric and imperial

--- composeApp/src/commonMain/kotlin/com/kronos/multiplatform/weatherapp/domain/repository/WeatherAlertsRemoteRepository.kt
+++ composeApp/src/commonMain/kotlin/com/kronos/multiplatform/weatherapp/domain/repository/WeatherAlertsRemoteRepository.kt
@@ -3,8 +3,6 @@ package com.kronos.multiplatform.weatherapp.domain.repository
 import com.kronos.multiplatform.weatherapp.core.result.Error
 import com.kronos.multiplatform.weatherapp.core.result.Result
 import com.kronos.multiplatform.weatherapp.domain.model.current.CurrentAlertsForecast
-import com.kronos.multiplatform.weatherapp.domain.model.current.CurrentForecast
-import com.kronos.multiplatform.weatherapp.domain.model.forecast.Forecast
 
 interface WeatherAlertsRemoteRepository {
     suspend fun getWeatherAlertsData(
